446dc958c865dccc4701ebb57375fd11e24d9bd6
GenJoeyy <bgirgis1000@gmail.com>
2023-10-05 22:39:43 +0000

    Oct 6, 2023, 12:39 AM

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 setInterval(function () {
   location.reload()
 }, 30000)
+
+function scrollToBottom() {
+  var sChat = document.getElementsByClassName("chat-scrollable-stegi")
+  sChat[0].lastElementChild.scrollIntoView()
+  var dChat = document.getElementsByClassName('chat-scrollable-di1araas')
+  dChat[0].lastElementChild.scrollIntoView()
+}
+
+window.onload = scrollToBottom
