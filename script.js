fcc0a5370b178ac3521089d94750835f0b1232e7
GenJoeyy <bgirgis1000@gmail.com>
2023-10-05 19:13:58 +0200

    update index.html

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function scrollToBottom(chatWindowId) {
+  var chatWindow = document.getElementById(chatWindowId);
+  chatWindow.scrollTop = chatWindow.scrollHeight;
+}
+
 setInterval(function () {
   location.reload()
+  scrollToBottom('chat-scrollable-stegi')
+  scrollToBottom('chat-scrollable-di1araas')
 }, 15000)
