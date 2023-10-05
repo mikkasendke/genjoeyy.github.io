3e8d5c0dc3a1fc16b2a911e30658488f54897e36
GenJoeyy <bgirgis1000@gmail.com>
2023-10-05 19:30:18 +0200

    update index.html

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-function scrollToBottom(chatWindowId) {
-  var chatWindow = document.getElementById(chatWindowId);
-  var lastChatMessage = chatWindow.querySelector('.chat-message:last-child');
-  if (lastChatMessage) {
-    lastChatMessage.scrollIntoView({ behavior: 'smooth' });
-  }
-}
-
-window.onload = function() {
-  scrollToBottom('chat-scrollable-stegi')
-  scrollToBottom('chat-scrollable-di1araas')
-}
-
+/*
 setInterval(function () {
   location.reload()
-  scrollToBottom('chat-scrollable-stegi')
-  scrollToBottom('chat-scrollable-di1araas')
 }, 15000)
+*/
