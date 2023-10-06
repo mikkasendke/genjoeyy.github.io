16b2c08fc4043c46caae7899f6caee3c880f5a52
GenJoeyy <bgirgis1000@gmail.com>
2023-10-06 17:04:21 +0200

    add viewerCount

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 app.get('/script.js', (req, res) => {
-    res.sendFile(path.join(__dirname, 'script.js'))
+    res.sendFile(path.join(__dirname, 'node_modules', 'script.js'))
 })
 app.get('/style.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'style.css'))
