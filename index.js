cee68edf5813502a376eab89ebf2a11a9e456711
GenJoeyy <bgirgis1000@gmail.com>
2023-10-06 18:46:51 +0000

    Oct 6, 2023, 8:46 PM

--- index.js
+++ index.js
@@ -140,10 +140,6 @@ app.get('/script.js', (req, res) => {
 app.get('/style.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'style.css'))
 })
-app.get('/index.js', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.js'))
-})
-
 app.get('/prime', (req, res) => {
     res.sendFile(path.join(__dirname,'prime.html'))
 })
